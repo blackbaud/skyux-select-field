422395f6a06040bc067e0afcf4bbc55df0d3d3e9
Conor Wright <conor.wright@blackbaud.com>
2019-02-20 15:30:48 -0500

    Added `blur` event to select field component. (#11)

--- e2e/select-field.e2e-spec.ts
+++ e2e/select-field.e2e-spec.ts
@@ -24,6 +24,7 @@ describe('Select field', () => {
   it('should match previous single mode screenshot with validation', (done) => {
     element(by.css('#screenshot-select-field-single-mode .sky-input-group.sky-btn')).click();
     element(by.css('.sky-modal-btn-close')).click();
+    element(by.css('body')).click();
     SkyHostBrowser.moveCursorOffScreen();
     expect('#screenshot-select-field-single-mode-wrapper').toMatchBaselineScreenshot(done, {
       screenshotName: 'select-field-single-mode-validation'
